771c583fcd7f5cd339d84f46772220c5b4a8ba98
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-01-20 23:50:26 +0530

    Update ArithmeticComputation.sh
    Compute a % b + c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -7,23 +7,3 @@ dict[r1]=$(( $a + ($b * $c) ));
 dict[r2]=$(( ($a * $b) + $c ));
 dict[r3]=$(( $c + ($a / $b) ));
 dict[r4]=$(( ($a % $b) + $c ));
-arr=( ${dict[@]} );
-echo "Array before sorting " ${arr[@]};
-for(( i=0;i<${#arr[@]};i++ ))
-do
-	for(( j=0;j<${#arr[@]}-$i-1;j++ ))
-	do
-		if [[ ${arr[j]} -gt ${arr[$(($j+1))]} ]]
-		then
-			temp=${arr[j]};
-			arr[$j]=${arr[$(($j+1))]};
-			arr[$(($j+1))]=$temp;
-		fi
-	done
-done
-echo "Array in Ascending Order " ${arr[@]};
-for(( i=${#arr[@]}-1;i>=0;i-- ))
-do
-	rev="$rev ${arr[i]}";
-done
-echo "Array in Descending Order " $rev;
